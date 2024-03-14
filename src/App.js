ee13c824c2ca842425b5ae7954ea00d949f80b11
Ubaidmwd <ubaidmwd@gmail.com>
2024-03-14 16:53:06 +0500

    services

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Header from "./components/Header";
 import Home from "./components/Home";
 import Footer from "./components/Footer";
 import Contact from "./components/Contact";
+import Services from "./components/Services";
 
 import "./styles/App.scss";
 import "./styles/header.scss";
@@ -18,6 +19,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/contact" element={<Contact />} />
+        <Route path="/services" element={<Services />} />
       </Routes>
       <Footer />
     </Router>
